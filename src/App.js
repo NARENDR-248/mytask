1a26e6cf3113ee9c22e80e77649d1b25cde06156
nani <naninarendra2000@mail.com>
2024-05-30 22:23:30 +0530

    ep-2

--- src/App.js
+++ src/App.js
@@ -19,6 +19,7 @@ function App() {
       completed: false,  // Fixed typo: "complited" to "completed"
     },
   ]);
+  const [show, setShow] = useState(true);
 
   const handleDelete = (id) => {
     const delete_list = task.filter((res) => {
@@ -28,16 +29,27 @@ function App() {
   };
 
   return (
-    <div className="App">
-      <h1>Task List</h1>
-      <ul>
-        {task.map((res) => {
+    <div className="flex flex-col items-center mx-auto justify-center shadow-lg w-6/12 p-4 m-10">
+      <h1 className="text-lg font-semibold border-neutral-700">Task List</h1>
+      <button
+        className="bg-red-700 rounded-md shadow-lg text-white px-3 py-2 mb-4"
+        onClick={() => setShow(!show)}
+      >
+        {show ? "Hide" : "Show"}
+      </button>
+      <ul className="w-full">
+        {show && task.map((res) => {
           return (
-            <li key={res.id}>
+            <li key={res.id} className="shadow-lg rounded-sm w-full p-6 flex justify-between mb-2">
               <span>
                 {res.id}_{res.name}
               </span>
-              <button onClick={() => handleDelete(res.id)}>Delete</button>
+              <button
+                className="bg-slate-500 rounded-md text-white p-2"
+                onClick={() => handleDelete(res.id)}
+              >
+                Delete
+              </button>
             </li>
           );
         })}
